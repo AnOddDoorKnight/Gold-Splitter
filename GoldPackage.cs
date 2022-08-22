7a372ff66420fff73fb2e3ec55c671c24fffd1d7
AnOddDoorKnight <gooeycuppy@live.com>
2022-08-21 17:52:36 -0700

    Added most support for operators

---
+++ GoldPackage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public record GoldPackage(int Platinum, int Gold, int Silver, int Copper)
+{
+	public const int copperRepresentative = 1, silverRepresentative = 10, goldRepresentative = 100, platinumRepresentative = 1000;
+	public static explicit operator int(GoldPackage package)
+		=> package.ToAllCopper().Copper;
+	public static GoldPackage operator +(GoldPackage packageLeft, GoldPackage packageRight)
+		=> new GoldPackage(0, 0, 0, packageLeft.ToAllCopper().Copper + packageRight.ToAllCopper().Copper).DisperseToAppropriateCoinValues();
+	public static GoldPackage operator +(GoldPackage packageLeft, int input)
+		=> new GoldPackage(0, 0, 0, packageLeft.ToAllCopper().Copper + input).DisperseToAppropriateCoinValues();
+	public static GoldPackage operator -(GoldPackage packageLeft, GoldPackage packageRight)
+		=> new GoldPackage(0, 0, 0, packageLeft.ToAllCopper().Copper - packageRight.ToAllCopper().Copper).DisperseToAppropriateCoinValues();
+	public static GoldPackage operator -(GoldPackage packageLeft, int input)
+		=> new GoldPackage(0, 0, 0, packageLeft.ToAllCopper().Copper - input).DisperseToAppropriateCoinValues();
+	public static GoldPackage operator /(GoldPackage package, int people)
+		=> new GoldPackage(0, 0, 0, package.ToAllCopper().Copper / people).DisperseToAppropriateCoinValues();
+	public static GoldPackage operator *(GoldPackage package, int times)
+		=> new GoldPackage(package.Platinum * times, package.Gold * times, package.Silver * times, package.Copper * times).DisperseToAppropriateCoinValues();
+
+	public static GoldPackage HumanInput()
+	{
+		Console.Write("Platinum: ");
+		int plat = int.Parse(Console.ReadLine() ?? "0");
+		Console.Write("Gold: ");
+		int gold = int.Parse(Console.ReadLine() ?? "0");
+		Console.Write("Silver: ");
+		int silver = int.Parse(Console.ReadLine() ?? "0");
+		Console.Write("Copper: ");
+		int copper = int.Parse(Console.ReadLine() ?? "0");
+		return new GoldPackage(plat, gold, silver, copper);
+	}
+
+	public GoldPackage ToAllCopper() => new(0, 0, 0, (Copper * copperRepresentative)
+		+ (Silver * silverRepresentative) + (Gold * goldRepresentative)
+		+ (Platinum * platinumRepresentative));
+	public GoldPackage DisperseToAppropriateCoinValues()
+	{
+		int copperTotal = ToAllCopper().Copper;
+		int platinumOut = copperTotal / platinumRepresentative;
+		copperTotal -= platinumOut * platinumRepresentative;
+		int goldOut = copperTotal / goldRepresentative;
+		copperTotal -= goldOut * goldRepresentative;
+		int silverOut = copperTotal / silverRepresentative;
+		copperTotal -= silverOut * silverRepresentative;
+		int copperOut = copperTotal;
+		return new GoldPackage(platinumOut, goldOut, silverOut, copperOut);
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,58 @@
-﻿using System;
-
-namespace GoldSplitter;
+﻿namespace GoldSplitter;
 using System;
 
 internal static class Program
 {
-	public static NamedAction[] actions =
+	public static NamedAction<GoldPackage>[] actions =
 	{
 		new ("Split", package =>
 		{
 			Console.WriteLine("Split with how many?");
 			int people = int.Parse(Console.ReadLine() ?? "0");
-			Console.WriteLine(package.Split(people));
+			Console.WriteLine(package / people);
 		}),
 		new ("To All Copper", package =>
 		{
 			Console.WriteLine(package.ToAllCopper().Copper);
-		})
+		}),
+		new ("Multiply", package =>
+		{
+			Console.WriteLine("Multiply how many times?");
+			int times = int.Parse(Console.ReadLine() ?? "0");
+			Console.WriteLine(package * times);
+		}),
+		new ("Add", package =>
+		{
+			Console.WriteLine("[1] Add with another package, [2] to add just copper.");
+			switch (int.Parse(Console.ReadLine() ?? "1"))
+			{
+				case 1:
+					Console.WriteLine(package + GoldPackage.HumanInput());
+					break;
+				case 2:
+					Console.Write("Copper: ");
+					Console.WriteLine(package + int.Parse(Console.ReadLine() ?? "0"));
+					break;
+				default:
+					throw new InvalidOperationException();
+			}
+		}),
+		new ("Remove", package =>
+		{
+			Console.WriteLine("[1] Remove with another package, [2] to remove just copper.");
+			switch (int.Parse(Console.ReadLine() ?? "1"))
+			{
+				case 1:
+					Console.WriteLine(package - GoldPackage.HumanInput());
+					break;
+				case 2:
+					Console.Write("Copper: ");
+					Console.WriteLine(package - int.Parse(Console.ReadLine() ?? "0"));
+					break;
+				default:
+					throw new InvalidOperationException();
+			}
+		}),
 	};
 	static void Main()
 	{
@@ -28,44 +64,4 @@ internal static class Program
 		Console.ReadLine();
 	}
 }
-public record NamedAction(string Name, Action<GoldPackage> Action);
-public record GoldPackage(int Platinum, int Gold, int Silver, int Copper)
-{
-	public const int copperRepresentative = 1, silverRepresentative = 10, goldRepresentative = 100, platinumRepresentative = 1000;
-	public static explicit operator int(GoldPackage package)
-		=> package.ToAllCopper().Copper;
-	public static GoldPackage HumanInput()
-	{
-		Console.Write("Platinum: ");
-		int plat = int.Parse(Console.ReadLine() ?? "0");
-		Console.Write("Gold: ");
-		int gold = int.Parse(Console.ReadLine() ?? "0");
-		Console.Write("Silver: ");
-		int silver = int.Parse(Console.ReadLine() ?? "0");
-		Console.Write("Copper: ");
-		int copper = int.Parse(Console.ReadLine() ?? "0");
-		return new GoldPackage(plat, gold, silver, copper);
-	}
-
-	public GoldPackage Split(int people)
-	{
-		int copper = ToAllCopper().Copper;
-		copper /= people;
-		return new GoldPackage(0, 0, 0, copper).DisperseToAppropriateCoinValues();
-	}
-	public GoldPackage ToAllCopper() => new(0, 0, 0, (Copper * copperRepresentative) 
-		+ (Silver * silverRepresentative) + (Gold * goldRepresentative) 
-		+ (Platinum * platinumRepresentative));
-	public GoldPackage DisperseToAppropriateCoinValues()
-	{
-		int copperTotal = ToAllCopper().Copper;
-		int platinumOut = copperTotal / platinumRepresentative;
-		copperTotal -= platinumOut * platinumRepresentative;
-		int goldOut = copperTotal / goldRepresentative;
-		copperTotal -= goldOut * goldRepresentative;
-		int silverOut = copperTotal / silverRepresentative;
-		copperTotal -= silverOut * silverRepresentative;
-		int copperOut = copperTotal;
-		return new GoldPackage(platinumOut, goldOut, silverOut, copperOut);
-	}
-}
+public record NamedAction<T>(string Name, Action<T> Action);
